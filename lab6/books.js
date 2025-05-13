a86f9ceb68659ecde5a9c67f91c2f15f6deac337
rodion <rodion.gorshkov.05@mail.ru>
2025-05-13 15:47:08 +0700

    last commit

---
+++ lab6/books.js
@@ -0,0 +1,112 @@
+const books = [
+    {
+        title: "JavaScript: The Good Parts",
+        author: "Douglas Crockford",
+        publicationYear: 2008,
+        publisher: "O'Reilly Media",
+        pages: 176,
+        price: 1500,
+        bestseller: true,
+        inStock: true,
+        genre: "Программирование"
+    },
+    {
+        title: "Clean Code",
+        author: "Robert C. Martin",
+        publicationYear: 2008,
+        publisher: "Pearson",
+        pages: 464,
+        price: 2200,
+        bestseller: true,
+        inStock: false,
+        genre: "Программирование"
+    },
+    {
+        title: "Design Patterns",
+        author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+        publicationYear: 1994,
+        publisher: "Addison-Wesley",
+        pages: 395,
+        price: 2500,
+        bestseller: true,
+        inStock: true,
+        genre: "Программирование"
+    },
+    {
+        title: "You Don't Know JS",
+        author: "Kyle Simpson",
+        publicationYear: 2015,
+        publisher: "O'Reilly Media",
+        pages: 160,
+        price: 1800,
+        bestseller: false,
+        inStock: true,
+        genre: "Программирование"
+    },
+    {
+        title: "Eloquent JavaScript",
+        author: "Marijn Haverbeke",
+        publicationYear: 2018,
+        publisher: "No Starch Press",
+        pages: 472,
+        price: 2000,
+        bestseller: true,
+        inStock: true,
+        genre: "Программирование"
+    },
+    {
+        title: "The Pragmatic Programmer",
+        author: "Andrew Hunt, David Thomas",
+        publicationYear: 1999,
+        publisher: "Addison-Wesley",
+        pages: 352,
+        price: 2300,
+        bestseller: true,
+        inStock: false,
+        genre: "Программирование"
+    },
+    {
+        title: "Structure and Interpretation of Computer Programs",
+        author: "Harold Abelson, Gerald Jay Sussman",
+        publicationYear: 1996,
+        publisher: "MIT Press",
+        pages: 657,
+        price: 2800,
+        bestseller: false,
+        inStock: true,
+        genre: "Программирование"
+    },
+    {
+        title: "Code Complete",
+        author: "Steve McConnell",
+        publicationYear: 2004,
+        publisher: "Microsoft Press",
+        pages: 914,
+        price: 3000,
+        bestseller: true,
+        inStock: true,
+        genre: "Программирование"
+    },
+    {
+        title: "Refactoring",
+        author: "Martin Fowler",
+        publicationYear: 2018,
+        publisher: "Addison-Wesley",
+        pages: 448,
+        price: 2400,
+        bestseller: false,
+        inStock: true,
+        genre: "Программирование"
+    },
+    {
+        title: "Introduction to Algorithms",
+        author: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+        publicationYear: 2009,
+        publisher: "MIT Press",
+        pages: 1312,
+        price: 3500,
+        bestseller: true,
+        inStock: true,
+        genre: "Алгоритмы"
+    }
+];
